5aca96845369368f0c970e4117eb052d2a13e4b3
BrenoMontanha <breno.montanha@estudante.ufjf.br>
2026-07-02 11:36:13 -0300

    Alterações na geometria e geração de malha para melhor resultado

--- windTurbineCase_SimpleFoam/create_mesh.sh
+++ windTurbineCase_SimpleFoam/create_mesh.sh
@@ -9,3 +9,6 @@ run_step blockMesh
 run_step surfaceFeatureExtract
 run_step snappyHexMesh -overwrite
 run_step checkMesh
+# run_step surfaceCheck constant/triSurface/propeller.stl
+
+grep -E "points:|faces:|cells:|hexahedra:|polyhedra:|prisms:|propeller|cylinder|non-orthogonality|Max skewness|Mesh OK|multiply connected|Failed" log.checkMesh
