fbd06bcda7c9b44eb2581f76e7bb6958c56e4038
Lucas <lucashn003@hotmail.com>
2026-09-09 14:52:53 -0300

    atualizando pimplefoam

--- windTurbineCase_PimpleFoam/create_mesh.sh
+++ windTurbineCase_PimpleFoam/create_mesh.sh
@@ -5,10 +5,20 @@ source "$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/scripts/lib_foam.sh"
 
 check_env
 
+# Remove campos auxiliares gerados por malhas anteriores
+rm -f \
+    0/cellLevel \
+    0/pointLevel \
+    0/cellToRegion \
+    0/nSurfaceLayers \
+    0/thickness \
+    0/thicknessFraction 
+
 run_step blockMesh
 run_step surfaceFeatureExtract
 run_step snappyHexMesh -overwrite
+run_step createBaffles -overwrite
 run_step checkMesh
-# run_step surfaceCheck constant/triSurface/propeller.stl
+# run_step surfaceCheck constant/triSurface/propeller_new.stl
 
 grep -E "points:|faces:|cells:|hexahedra:|polyhedra:|prisms:|propeller|cylinder|non-orthogonality|Max skewness|Mesh OK|multiply connected|Failed" log.checkMesh
